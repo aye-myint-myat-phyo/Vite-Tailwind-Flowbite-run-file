2b7fc230c7cc43fdd15957d1691fbd2a0c375a4a
Aye Myint Myat Phyo <ayemyintmyatphyo.dm@gmail.com>
2023-12-08 00:42:41 +0630

    aos test

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+import Typed from './node_modules/typed.js';
+import AOS from 'aos';
 import './style.css'
+
+const typeUi = document.querySelector("#typeUi");
+
+const typed = new Typed(typeUi,{
+    strings : ["<span class='text-blue-500'>Companies</span>", "<span class='text-gray-500'>House</span>", "Organization"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    contentType: 'html',
+    loop : true,
+})
+
+AOS.init();
